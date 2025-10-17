62e582a9949c6366592de6210e42cde1bb7c167b
Mahesh Yadav <maheshyadav49847@gmail.com>
2025-10-17 19:13:27 +0000

    side bar thoda chuda kro

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -7,7 +7,7 @@ export default function AdminLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+    <div className="grid min-h-screen w-full md:grid-cols-[260px_1fr] lg:grid-cols-[320px_1fr]">
       <aside className="hidden border-r bg-card md:block">
         <AppSidebarNav />
       </aside>
