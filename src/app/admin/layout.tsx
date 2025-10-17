00d8ebfeac5115e32ae95170b45d6c67c5798966
Mahesh Yadav <maheshyadav49847@gmail.com>
2025-10-17 14:39:22 +0000

    gradient puple aur white ka combination hona chahiye

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -8,12 +8,12 @@ export default function AdminLayout({
 }>) {
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <aside className="hidden border-r bg-muted/40 md:block">
+      <aside className="hidden border-r bg-card md:block">
         <AppSidebarNav />
       </aside>
       <div className="flex flex-col">
         <AppHeader />
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-gradient-to-br from-background to-white">
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-gradient-to-br from-purple-50 to-white">
           {children}
         </main>
       </div>
